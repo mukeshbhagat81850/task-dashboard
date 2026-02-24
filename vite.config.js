35928739a680b93306ac16257d6c377c218bc972
mukeshbhagat81850 <mukeshbhagat81850@gmail.com>
2026-02-24 15:45:06 +0530

    Updated vite config for proper GitHub Pages hosting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/task-dashboard/",
+  base: "/task-dashboard/", // ⚠ repo name same hona chahiye
   plugins: [react(), tailwindcss()],
-})
+  build: {
+    outDir: "dist",
+    sourcemap: false, // production me source map off
+  },
+});
